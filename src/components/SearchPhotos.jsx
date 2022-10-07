6b26d3676dd07618a9504050935cb76fa2db25bc
zaljifri <zainab.aljifri@gmail.com>
2022-10-07 18:10:13 +0300

    Add pagintation

--- src/components/SearchPhotos.jsx
+++ src/components/SearchPhotos.jsx
@@ -1,38 +1,51 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import QueryInput from "./QueryInput";
 import Results from "./Results";
+import { Pagination } from "@mui/material";
 
 export default function SearchPhotos() {
   const [query, setQuery] = useState("");
   const [pics, setPics] = useState([]);
-  const Access_Key = 'BMHgcnpJzN0JOSOwbmfG7jRNqiGE2f62aISEIS2XVZE';
-  const Access_Key2 = '30343985-5d8c3a7a0f500ff0d60685014';
-  const fetchRequest = async (e) => {
-    e.preventDefault();
-    const data = await fetch(
-      // `https://pixabay.com/api/?key=${Access_Key2}&q=${query}&per_page=50`
-      `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=${Access_Key}&per_page=30`
-    );
-    const dataJ = await data.json();
-    const result = dataJ.results;
-    // setPics(dataJ.hits);
-    setPics(result);
+  const [page, setPage] = useState(1);
+  const [pageCount, setPageCount] = useState(10);
+
+  const handlePageChange = (event, value) => {
+    setPage(value);
   };
-  
+  const handleQueryClear = () => {
+    setQuery('');
+    setPage(1);
+  };
+  const handleQueryChange = (e) => {
+    setQuery(e.target.value)
+    setPage(1);
+  };
+  const handlePageCount = (json) => {
+    setPics(json.hits);
+    setPageCount(Math.round(json.totalHits/10));
+    console.log(Math.round(json.totalHits/10));
+  };
+  // const Access_Key1 = 'BMHgcnpJzN0JOSOwbmfG7jRNqiGE2f62aISEIS2XVZE';
+  const Access_Key2 = '30343985-5d8c3a7a0f500ff0d60685014'
 
   useEffect(() => {
     fetch(
-      // `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=${Access_Key}&per_page=10`
-      `https://pixabay.com/api/?key=${Access_Key2}&q=${query}&per_page=30`
-      )
+      // `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=${Access_Key1}&per_page=10`
+      `https://pixabay.com/api/?key=${Access_Key2}&q=${query}&per_page=10&page=${page}`
+    )
       .then((response) => response.json())
       // .then((json) => setPics(json.results));
-      .then((json) => setPics(json.hits));
-  }, [query]);
+      .then((json) => handlePageCount(json))
+      // .then((json) =>  setPageCount(json.totalHits/10));
+    //   console.log(pics.length);
+    console.log(page);
+    console.log(query);
+  }, [page, query]);
   return (
     <>
-    <QueryInput value={query} onClick={() => setQuery('')} onChange={(e) => setQuery(e.target.value)}/>
-      <Results array={pics}/>
+      <QueryInput value={query} onClick={handleQueryClear} onChange={handleQueryChange} />
+      <Results array={pics} />
+      <Pagination className="pagintation" count={pageCount} page={page} onChange={handlePageChange} />
     </>
   );
 }
